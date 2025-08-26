411801fbcf20eff27e6f5dd43dd6a1f60cf61688
Joachim Renevier <renevierjoachim@gmail.com>
2025-08-26 19:43:26 +0200

    fix: upadate develop

--- app/src/domain/interface_repositories/user_repository.py
+++ app/src/domain/interface_repositories/user_repository.py
@@ -1,28 +1,27 @@
-from typing import List, Optional
 from abc import ABC, abstractmethod
-from app.src.domain.entities.tag import Tag
+from app.src.domain.entities.user import User
 
 class UserRepository(ABC):
     @abstractmethod
-    def create_user(self, tag: Tag) -> Tag:
+    def create_user(self, user: User) -> User:
         pass
 
     @abstractmethod
-    def select_users(self, offset: Optional[int] = None, limit: Optional[int] = None) -> List[Optional[Tag]]:
+    def select_users(self, offset: int | None = None, limit: int | None = None) -> list[User | None]:
         pass
 
     @abstractmethod
-    def select_user_by_id(self, tag_id: int) -> Tag:
+    def select_user_by_id(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    def select_user_by_src_address(self, tag_src_address: str) -> Tag:
+    def select_user_by_src_address(self, user_src_address: str) -> User:
         pass
 
     @abstractmethod
-    def update_user(self, tag_id: int) -> Tag:
+    def update_user(self, user_id: int) -> User:
         pass
 
     @abstractmethod
-    def delete_user(self, tag_id: int) -> bool:
+    def delete_user(self, user_id: int) -> bool:
         pass
