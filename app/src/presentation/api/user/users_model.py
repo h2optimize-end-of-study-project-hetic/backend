6a727642cebf1285e76360cc54f2f89755edfe38
Joachim Renevier <renevierjoachim@gmail.com>
2025-08-26 20:12:45 +0200

    rebase

--- app/src/presentation/api/user/users_model.py
+++ app/src/presentation/api/user/users_model.py
@@ -1,36 +1,38 @@
-from typing import Optional, List
+
 from pydantic import BaseModel, Field
+
 from app.src.domain.entities.role import Role
+
 
 class UserBaseModel(BaseModel):
     email: str = Field(..., min_length=5, max_length=255)
     firstname: str = Field(..., min_length=1, max_length=255)
     lastname: str = Field(..., min_length=1, max_length=255)
-    phone_number: Optional[str] = None
-    role: Role = Role.GUEST
+    phone_number: str | None = None
+    role: Role = Role.guest.value
     is_active: bool = True
 
 class UserCreateModel(UserBaseModel):
     password: str = Field(..., min_length=6)
     salt: str
-    secret_2fa: Optional[str] = None
+    secret_2fa: str | None = None
 
 class UserUpdateModel(BaseModel):
-    email: Optional[str] = Field(default=None, min_length=5, max_length=255)
-    firstname: Optional[str] = Field(default=None, min_length=1, max_length=255)
-    lastname: Optional[str] = Field(default=None, min_length=1, max_length=255)
-    phone_number: Optional[str] = None
-    role: Optional[Role] = None
-    is_active: Optional[bool] = None
-    password: Optional[str] = Field(default=None, min_length=6)
-    salt: Optional[str] = None
-    secret_2fa: Optional[str] = None
+    email: str | None = Field(default=None, min_length=5, max_length=255)
+    firstname: str | None = Field(default=None, min_length=1, max_length=255)
+    lastname: str | None = Field(default=None, min_length=1, max_length=255)
+    phone_number: str | None = None
+    role: Role | None = None
+    is_active: bool | None = None
+    password: str | None = Field(default=None, min_length=6)
+    salt: str | None = None
+    secret_2fa: str | None = None
 
 class UserModel(UserBaseModel):
     id: int
 
 class PaginatedUsersModel(BaseModel):
-    data: List[UserModel]
+    data: list[UserModel]
     count: int
     offset: int
     limit: int
