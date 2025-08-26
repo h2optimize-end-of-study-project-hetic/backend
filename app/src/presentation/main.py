3fb00ff98c2dab29b2665d4f79a052742f785c62
LinelinLove <chrisline.lin@outlook.fr>
2025-08-26 19:55:50 +0200

    crud event

--- app/src/presentation/main.py
+++ app/src/presentation/main.py
@@ -9,6 +9,8 @@ from app.src.presentation.core.config import settings
 from app.src.presentation.core.open_api_tags import OpenApiTags
 from app.src.presentation.core.open_api_events import OpenApiEvents
 from app.src.presentation.core.open_api_maps import OpenApiMaps
+from app.src.presentation.core.open_api_events import OpenApiEvents
+from app.src.presentation.core.open_api_maps import OpenApiMaps
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -49,14 +51,6 @@ app = FastAPI(
         {
             "name": OpenApiTags.room,
             "description": "Gestion des salle au sein des organisations",
-        },
-        {
-            "name": OpenApiMaps.map,
-            "description": "Les maps correspondent aux images du plan d'un étage d'un bâtiment",
-        },
-        {
-            "name": OpenApiEvents.event,
-            "description": "Les événements représentent des activités associées à une salle.",
         },
         {
             "name": OpenApiMaps.map,
